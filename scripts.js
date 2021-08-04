65627a60a33ae5cb76bebc119b7cfeab431325b9
UsernameSeeking <71772311+UsernameSeeking@users.noreply.github.com>
2021-08-04 16:33:26 +0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+'use strict'
+
+function start() {
+    var i;
+    var count = 0;
+    var check = 1;
+    var mainl = setInterval(function() {
+        if (check >= 0) {
+            var b1 = document.getElementById('column1');
+            var a1 = document.getElementsByClassName('row1');
+            i = Math.floor(Math.random() * 2);
+            b1.appendChild(a1[i]);
+        }
+        if (check >= -2) {
+            var b2 = document.getElementById('column2');
+            var a2 = document.getElementsByClassName('row2');
+            i = Math.floor(Math.random() * 2);
+            b2.appendChild(a2[i]);
+        }
+        if (check >= -4) {
+            var b3 = document.getElementById('column3');
+            var a3 = document.getElementsByClassName('row3');
+            i = Math.floor(Math.random() * 2);
+            b3.appendChild(a3[i]);
+        }
+        count ++;
+        if (count >= 30 && count < 38 && check > 0) {
+            check = Math.floor(Math.random() * 5);
+        }
+        else {
+            if (check <= 0) {
+                check --;
+                if (check == -5) clearInterval(mainl);
+            }
+            if (count == 38) check = 0;
+        }
+        //a[0].style.animation = "test 1s linear";    
+    }, 100);
+}
